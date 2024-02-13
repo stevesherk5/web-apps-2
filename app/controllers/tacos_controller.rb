9a599ebc03523516f9b8a2cd288879b85d66cddb
Steve Sherk <steven.sherk@kellogg.northwestern.edu>
2024-02-13 17:30:45 +0000

    class 7

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -2,6 +2,7 @@ class TacosController < ApplicationController
 
   def index
     # render :template => "tacos/index"
+    @fillings = ["Carnitas", "Al Pastor", "Steak", "Fish", "Veggie", "Mushroom"]
   end
 
 end
